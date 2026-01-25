2b6e6d7c9b9dcf70e57d64bc781bd8b77d49552c
Antuan Bobochek <81513032+Avangard-22@users.noreply.github.com>
2026-01-25 23:19:55 +0300

    Delete kosmicheskiy-klikker-server directory

--- kosmicheskiy-klikker-server/src/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-require('dotenv').config();
-
-module.exports = {
-  PORT: process.env.PORT || 3000,
-  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '',
-  NODE_ENV: process.env.NODE_ENV || 'development'
-};
